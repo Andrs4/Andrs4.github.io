6010df38c29fabb511da35b5c509ffed66fc210b
WDD230 Test student account <asorucomamani@gmail.com>
2021-01-12 22:19:16 -0400

    updated

--- date.js
+++ date.js
@@ -2,6 +2,6 @@ window.addEventListener('load', (event)=>{
     const update = document.querySelector('#currentDate');
     update.textContent = document.lastModified;
 
-    const year = document.querySelector('#copyrightyear');
+    const year = document.querySelector('#currentYear');
     year.textContent = new Date().getFullYear();
 })
