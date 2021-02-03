68368b22e5d61a55cad3ed21babb9e668303812e
WDD230 Test student account <asorucomamani@gmail.com>
2021-02-03 17:11:57 -0400

    working on the main part of the preston page

---
+++ lesson5/js/date.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+}
+
+
+window.addEventListener('load', (event)=>{
+    
+    /*
+    const update = document.querySelector('#currentDate');
+    update.textContent = document.lastModified;
+    */
+
+    const update = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-us', update);
+
+
+    const year = document.querySelector('#currentYear');
+    year.textContent = new Date().getFullYear();
+})
