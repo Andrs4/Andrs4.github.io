0b9a3692011a8ce9ae0bfd5f379b7dbb06cf9ede
WDD230 Test student account <asorucomamani@gmail.com>
2021-02-18 00:45:14 -0400

    JS fixed but not working yet

--- lesson7/lazyload/js/script.js
+++ lesson7/lazyload/js/script.js
@@ -1,15 +1,15 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px -500px 0px"
-};
-
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttibute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');
     };
+};
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 200px 0px"
 };
 
 imagesToLoad.forEach((img) => {
@@ -18,13 +18,13 @@ imagesToLoad.forEach((img) => {
 
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
-        items.forEach(item) => {
+        items.forEach((item) => {
             if(item.isIntersecting) {
                 loadImages(item.target);
                 observer.unobserve(item.target);
             }
         });
-    } imgOptions);
+    }, imgOptions);
     imagesToLoad.forEach((img) => {
         observer.observer(img);
     });
