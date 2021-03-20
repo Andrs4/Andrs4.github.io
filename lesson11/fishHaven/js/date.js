ed39f92f51957f79d61fa60c28a3b2c0b82c00b1
WDD230 Test student account <asorucomamani@gmail.com>
2021-03-20 11:00:17 -0400

    Fish haven and Soda Springs pages added

---
+++ lesson11/fishHaven/js/date.js
@@ -0,0 +1,33 @@
+
+/*It hides the menu bar when its size is smaller and let it show when clicking a specific element*/
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+}
+
+
+window.addEventListener('load', (event)=>{
+    
+    /*
+    const update = document.querySelector('#currentDate');
+    update.textContent = document.lastModified;
+    */
+
+    const update = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-us', update);
+
+
+    const year = document.querySelector('#currentYear');
+    year.textContent = new Date().getFullYear();
+});
+
+/*======================== It Displays a banner on fridays =================*/
+
+window.onload = function toggleAnnouncement() {
+    let day = new Date();
+    let actual = day.getDay();
+    /*==== is tried to check for friday and if it's not it should hide the aside element=====*/
+    if (actual != 5) {    
+        document.getElementById("pancakes").classList.toggle("hide");
+    }
+}
