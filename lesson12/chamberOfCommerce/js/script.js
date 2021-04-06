24fdcddda33c1b65edc3f198383654263f93112b
WDD230 Test student account <asorucomamani@gmail.com>
2021-04-06 18:37:33 -0400

    finishing header and footer for all pages

--- lesson12/chamberOfCommerce/js/script.js
+++ lesson12/chamberOfCommerce/js/script.js
@@ -1,3 +1,8 @@
+/*let d = new Date();
+console.log(d);
+*/
+
+
 window.addEventListener('load', (event)=>{
 
     const update = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
@@ -13,9 +18,21 @@ function toggleMenu() {
     window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 }
 
+/*==== Tries to change the background color for the header when scrolling down ====*/
 function addingBackground() {
     document.getElementsByClassName("top").classList.toggle("has-background");
     window.onmouseover = () => {
         if (window.innerHeight < 50) mainnav.classList.remove("has-background")
     };
+}
+
+
+/*==== Add a upcoming event to the page ====*/
+window.onload = function toggleEvents() {
+    let day = new Date();
+    let actual = day.getDate();
+    /*==== is tried to check for friday and if it's not it should hide the aside element=====*/
+    if (actual >= 11) {    
+        document.getElementById("elections").classList.toggle("hide");
+    }
 }
