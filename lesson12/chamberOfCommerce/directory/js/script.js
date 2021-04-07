42ef9a548727d96230033c0b383f24408620025a
WDD230 Test student account <asorucomamani@gmail.com>
2021-04-07 14:12:45 -0400

    json images

--- lesson12/chamberOfCommerce/directory/js/script.js
+++ lesson12/chamberOfCommerce/directory/js/script.js
@@ -5,7 +5,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        //console.table(jsonObject);  // temporary checking for valid response and data parsing
         const business = jsonObject['business'];
 
         const cards = document.querySelector(".cards")
@@ -15,22 +15,22 @@ fetch(requestURL)
             let h2 = document.createElement("h2");
             let p1 = document.createElement("p");
             let p2 = document.createElement("p");
-            let prophetimg = document.createElement("img");
+            let businessimg = document.createElement("img");
             
             // template literals
             h2.textContent = `${eachB.name} ${eachB.fullname}`;
-            p1.textContent = `Date of Birth: ${prophet.birthdate}`;
-            p2.textContent = `Place of Birth: ${prophet.birthplace}`;
-            prophetimg.setAttribute('src' , prophet.imageurl);
-            prophetimg.setAttribute("alt", `Prophet ${prophet.name} ${prophet.lastname} image`);
-            prophetimg.setAttribute("loading", "lazy")
-            prophetimg.style.boxShadow = '0 0 30px #333';
-            prophetimg.style.width = '200px';
+            p1.textContent = `Date of Birth: ${eachB.city}`;
+            p2.textContent = `Place of Birth: ${eachB.business}`;
+            businessimg.setAttribute('src' , eachB.image);
+            businessimg.setAttribute("alt", `${eachB.image} image`);
+            businessimg.setAttribute("loading", "lazy")
+            businessimg.style.boxShadow = '0 0 30px #333';
+            businessimg.style.width = '200px';
             
             card.append(h2);
             card.append(p1);
             card.append(p2);
-            card.append(prophetimg);
+            card.append(businessimg);
             cards.append(card);
       });
 
