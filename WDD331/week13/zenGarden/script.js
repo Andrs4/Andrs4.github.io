b50b2a3156070ca9d38e72cffae1e640ca2946d2
WDD230 Test student account <asorucomamani@gmail.com>
2021-07-16 00:49:56 -0400

    WDD331 final project images incomplete

---
+++ WDD331/week13/zenGarden/script.js
@@ -0,0 +1,21 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+var tag = document.createElement("p");
+var new_image = document.createElement("img")
+new_image.src='images/image1.JPG'
+var text = document.createTextNode("Tutorix is the best e-learning platform");
+tag.appendChild(text);
+var element = document.getElementById("zen-explanation");
+element.appendChild(tag);
+element.appendChild(new_image)
